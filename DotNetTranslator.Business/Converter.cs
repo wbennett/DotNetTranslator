42768e6ba1b83a943a7c937f3633d19b298c3ebd
Paul Bennett <wbennett@cse.unl.edu>
2014-01-06 00:08:27 -0600

    Updated readme and added license.

--- DotNetTranslator.Business/Converter.cs
+++ DotNetTranslator.Business/Converter.cs
@@ -1,4 +1,24 @@
-﻿using System.IO;
+﻿//***********************************************
+// Author: Paul Bennett wm.paul.bennett@gmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0 
+//
+// THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES
+// OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+// INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES
+// OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+//
+// See the Apache 2 License for the specific language
+// governing permissions and limitations under the License.
+//
+// *********************************************************
+using System.IO;
 using System.Text;
 
 namespace DotNetTranslator.Business
@@ -7,18 +27,46 @@ namespace DotNetTranslator.Business
     {
         private static readonly VisualBasicToCSharpConverter.Converting.Converter visualBasicToCSharpConverter =
             new VisualBasicToCSharpConverter.Converting.Converter();
+        private static readonly CSharpToVisualBasicConverter.Converting.Converter csharpToVisualBasicConverter = 
+            new CSharpToVisualBasicConverter.Converting.Converter();
         /// <summary>
-        /// Convert a vb file to a CSharp equivalent.
+        /// Convert a vb file to a C# memory stream.
+        /// Usage: 
+        /// using(var stream = Converter.ConvertVbToCSharp(filePath))
+        /// {
+        /// ....
+        /// }
         /// </summary>
-        /// <param name="stream"></param>
-        /// <returns></returns>
-        public static Stream ConvertVBToCSharp(string file)
+        /// <param name="file">String containing the file path of the input source.</param>
+        /// <returns>File stream of the converted code.</returns>
+        public static Stream ConvertVbtoCSharp(string file)
         {
-            var tree = Roslyn.Compilers.VisualBasic.SyntaxTree.ParseFile(file);
-            var csharpCode = visualBasicToCSharpConverter.Convert(tree);
+            var vbAst = Roslyn.Compilers.VisualBasic.SyntaxTree.ParseFile(file);
+            var csharpAst = visualBasicToCSharpConverter.Convert(vbAst);
             var rStream = new MemoryStream(
                 Encoding.UTF8.GetBytes(
-                    csharpCode.ToFullString()
+                    csharpAst.ToFullString()
+                ));
+            return rStream;
+        }
+
+        /// <summary>
+        /// Convert a C# source file to a VB memory stream.
+        /// Usage: 
+        /// using(var stream = Converter.ConvertCSharpToVb(filePath))
+        /// {
+        /// ....
+        /// }
+        /// </summary>
+        /// <param name="file">String containing the file path of the input source.</param>
+        /// <returns>File stream of the converted code.</returns>
+        public static Stream ConvertCSharpToVb(string file)
+        {
+            var vbAst = Roslyn.Compilers.VisualBasic.SyntaxTree.ParseFile(file);
+            var csharpAst = visualBasicToCSharpConverter.Convert(vbAst);
+            var rStream = new MemoryStream(
+                Encoding.UTF8.GetBytes(
+                    csharpAst.ToFullString()
                 ));
             return rStream;
         }
--- DotNetTranslator/Program.cs
+++ DotNetTranslator/Program.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿//***********************************************
+// Author: Paul Bennett wm.paul.bennett@gmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of
+// the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0 
+//
+// THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES
+// OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+// INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES
+// OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY OR NON-INFRINGEMENT.
+//
+// See the Apache 2 License for the specific language
+// governing permissions and limitations under the License.
+//
+// *********************************************************
+using System;
 using System.IO;
 using DotNetTranslator.Business;
 
@@ -21,7 +41,7 @@ namespace DotNetTranslator
             }
 
 
-            using (var stream = Converter.ConvertVBToCSharp(args[0]))
+            using (var stream = Converter.ConvertVbtoCSharp(args[0]))
             using (var fileStream = new FileStream(args[1],FileMode.CreateNew))
             {
                 stream.Seek(0, SeekOrigin.Begin);
