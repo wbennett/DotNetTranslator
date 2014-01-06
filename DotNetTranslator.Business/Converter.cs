098ebabddd4488f251ac6416711428f085d58c75
Paul Bennett <wbennett@cse.unl.edu>
2014-01-05 22:59:29 -0600

    Initial commit, includes ability to convert a visual basic program to the C# version.

---
+++ DotNetTranslator.Business/Converter.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Text;
+
+namespace DotNetTranslator.Business
+{
+    public class Converter
+    {
+        private static readonly VisualBasicToCSharpConverter.Converting.Converter visualBasicToCSharpConverter =
+            new VisualBasicToCSharpConverter.Converting.Converter();
+        /// <summary>
+        /// Convert a vb file to a CSharp equivalent.
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static Stream ConvertVBToCSharp(string file)
+        {
+            var tree = Roslyn.Compilers.VisualBasic.SyntaxTree.ParseFile(file);
+            var csharpCode = visualBasicToCSharpConverter.Convert(tree);
+            var rStream = new MemoryStream(
+                Encoding.UTF8.GetBytes(
+                    csharpCode.ToFullString()
+                ));
+            return rStream;
+        }
+    }
+}
---
+++ DotNetTranslator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using DotNetTranslator.Business;
+
+namespace DotNetTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2 ||
+                !File.Exists(args[0]))
+            {
+                Console.WriteLine("Usage: GarbageToGold.exe <path/to/vb file> <path/to/csharp file>");
+                return;
+            }
+            
+            if (File.Exists(args[1]))
+            {
+                Console.WriteLine("Output file exists, please remove to output new file.");
+            }
+
+
+            using (var stream = Converter.ConvertVBToCSharp(args[0]))
+            using (var fileStream = new FileStream(args[1],FileMode.CreateNew))
+            {
+                stream.Seek(0, SeekOrigin.Begin);
+                stream.CopyTo(fileStream);
+            }
+        }
+    }
+}
