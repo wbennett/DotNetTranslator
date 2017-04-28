9dac0d5ea66b6c3b2fe79ac846e1fe69681c52fc
Wenzel, Toni <toni.wenzel@chg-meridian.de>
2017-04-28 09:48:55 +0200

    Add method to convert strings

--- DotNetTranslator.Business/Converter.cs
+++ DotNetTranslator.Business/Converter.cs
@@ -29,6 +29,7 @@ namespace DotNetTranslator.Business
             new VisualBasicToCSharpConverter.Converting.Converter();
         private static readonly CSharpToVisualBasicConverter.Converting.Converter csharpToVisualBasicConverter = 
             new CSharpToVisualBasicConverter.Converting.Converter();
+        
         /// <summary>
         /// Convert a vb file to a C# memory stream.
         /// Usage: 
@@ -51,6 +52,20 @@ namespace DotNetTranslator.Business
         }
 
         /// <summary>
+        /// Convert a vb code to a C# string.
+        /// Usage: 
+        /// var csharpCode = Converter.ConvertVbtoCSharpString(vbCode);
+        /// </summary>
+        /// <param name="content">String containing the CSharp code.</param>
+        /// <returns>File stream of the converted code.</returns>
+        public static string ConvertVbtoCSharpString(string content)
+        {
+            var vbAst = Roslyn.Compilers.VisualBasic.SyntaxTree.ParseText(content);
+            var csharpAst = visualBasicToCSharpConverter.Convert(vbAst);
+            return csharpAst.ToFullString();
+        }
+
+        /// <summary>
         /// Convert a C# source file to a VB memory stream.
         /// Usage: 
         /// using(var stream = Converter.ConvertCSharpToVb(filePath))
@@ -69,6 +84,20 @@ namespace DotNetTranslator.Business
                     csharpAst.ToFullString()
                 ));
             return rStream;
+        }
+
+        /// <summary>
+        /// Convert a C# source file to a VB memory stream.
+        /// Usage: 
+        /// var vbCode = Converter.ConvertVbtoCSharpString(csharpCode);
+        /// </summary>
+        /// <param name="content">String containing the CSharp code.</param>
+        /// <returns>File stream of the converted code.</returns>
+        public static string ConvertCSharpToVbString(string content)
+        {
+            var vbAst = Roslyn.Compilers.VisualBasic.SyntaxTree.ParseText(content);
+            var csharpAst = visualBasicToCSharpConverter.Convert(vbAst);
+            return csharpAst.ToFullString();
         }
     }
 }
